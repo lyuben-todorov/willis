bb0141e2e17cb3a22ba5d2e213cfad46890777d4
Lyuben Todorov <lyuben.todorov@gmail.com>
2020-10-21 12:33:26 +0300

    Add samples

--- src/tests.rs
+++ src/tests.rs
@@ -13,57 +13,62 @@ mod tests {
     #[test]
     fn test_throughput_single_node() {
         let runs: usize = 1000;
+        let samples = 20;
         let socket_addr = get_adrr(8878);
-        let timer = Instant::now();
-        for i in 0..runs {
-            let req = ClientReq::publ(CLIENT_ID, i.to_string().as_bytes().to_vec());
-            let res = input(&socket_addr, req);
-            match res {
-                ClientRes::Error { message: _ } => { assert!(false) }
-                _ => {}
+        for run in 0..samples {
+            let timer = Instant::now();
+            for i in 0..runs {
+                let req = ClientReq::publ(CLIENT_ID, i.to_string().as_bytes().to_vec());
+                let res = input(&socket_addr, req);
+                match res {
+                    ClientRes::Error { message: _ } => { assert!(false) }
+                    _ => {}
+                }
             }
-            if i % (runs / 10) == 0 { println!("{}%", 100 * i / runs) }
+            println!("{}. Test took {:.2?}, avg {} ops/s", run, timer.elapsed(), runs as f64 / timer.elapsed().as_secs_f64())
         }
-        println!("Test took {:.2?}, avg {} ops/s", timer.elapsed(), runs as f64 / timer.elapsed().as_secs_f64())
     }
 
     #[test]
     fn test_throughput_two_nodes() {
-        let runs: usize = 1000;
+        let runs: usize = 2000;
+        let samples = 20;
         let half = runs / 2;
         let socket_addr1 = get_adrr(8878);
         let socket_addr2 = get_adrr(8879);
 
-        let timer = Instant::now();
-        let thread1 = std::thread::spawn(move || {
-            for i in 0..half {
-                let req = ClientReq::publ(CLIENT_ID, i.to_string().as_bytes().to_vec());
-                let res = input(&socket_addr1, req);
-                match res {
-                    ClientRes::Error { message: _ } => { assert!(false) }
-                    _ => {}
+        for run in 0..samples {
+            let timer = Instant::now();
+            let thread1 = std::thread::spawn(move || {
+                for i in 0..half {
+                    let req = ClientReq::publ(CLIENT_ID, i.to_string().as_bytes().to_vec());
+                    let res = input(&socket_addr1, req);
+                    match res {
+                        ClientRes::Error { message: _ } => { assert!(false) }
+                        _ => {}
+                    }
                 }
-            }
-        });
-        let thread2 = std::thread::spawn(move || {
-            for i in half..runs {
-                let req = ClientReq::publ(CLIENT_ID, i.to_string().as_bytes().to_vec());
-                let res = input(&socket_addr2, req);
-                match res {
-                    ClientRes::Error { message: _ } => { assert!(false) }
-                    _ => {}
+            });
+            let thread2 = std::thread::spawn(move || {
+                for i in half..runs {
+                    let req = ClientReq::publ(CLIENT_ID, i.to_string().as_bytes().to_vec());
+                    let res = input(&socket_addr2, req);
+                    match res {
+                        ClientRes::Error { message: _ } => { assert!(false) }
+                        _ => {}
+                    }
                 }
-            }
-        });
-        thread1.join().unwrap();
-        thread2.join().unwrap();
+            });
+            thread1.join().unwrap();
+            thread2.join().unwrap();
 
-        let wait_req = ClientReq::WaitUntilClear { client_id: CLIENT_ID };
-        input(&socket_addr1, wait_req);
-        let wait_req = ClientReq::WaitUntilClear { client_id: CLIENT_ID };
-        input(&socket_addr2, wait_req);
+            let wait_req = ClientReq::WaitUntilClear { client_id: CLIENT_ID };
+            input(&socket_addr1, wait_req);
+            let wait_req = ClientReq::WaitUntilClear { client_id: CLIENT_ID };
+            input(&socket_addr2, wait_req);
 
-        println!("Test took {:.2?}, avg {} ops/s", timer.elapsed(), (runs as f64) / timer.elapsed().as_secs_f64())
+            println!("{}. Test took {:.2?}, avg {} ops/s", run, timer.elapsed(), (runs as f64) / timer.elapsed().as_secs_f64())
+        }
     }
 }
 
